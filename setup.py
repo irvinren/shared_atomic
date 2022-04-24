a28e29d045c164064963c57577eb1651a7073bd2
Xiquan Ren <brightdayrxq@yahoo.com>
2022-04-24 19:06:42 +0800

    refactored

--- setup.py
+++ setup.py
@@ -3,33 +3,42 @@ from distutils.core import setup as distutils_setup
 from setuptools import setup as setuptools_setup
 from shared_atomic import atomic_setup
 
+__version__="2.0.1"
+__package_name__='shared_atomic'
+__author__="Xiquan Ren"
+__author_email__="xiquanren@yandex.com"
+__description__="Shared atomicity with multiprocessing/multithreads shared ctypes"
+__url__='https://github.com/irvinren/shared_atomic.git'
+__packages__=['shared_atomic']
 
 with open("readme.rst") as f:
     readme = f.read()
 
 if sys.platform in('darwin','linux'):
     distutils_setup(
-        name='shared_atomic',
-        version="2.0.0",
-        author="Xiquan Ren",
-        author_email="xiquanren@yandex.com",
-        description="Shared atomicity with multiprocessing/multithreads shared ctypes",
-        url='https://github.com/irvinren/shared_atomic.git',
+        name=__package_name__,
+        version=__version__,
+        author=__author__,
+        author_email=__author_email__,
+        description=__description__,
+        url=__url__,
         long_description=readme,
         ext_modules=[atomic_setup.ffi.distutils_extension()],
-        packages=['shared_atomic'],
+        packages=__packages__,
         data_files=[('shared_atomic',['shared_atomic/atomic_csource.c',
                                         'shared_atomic/atomic_csource.h']),
                     ('', ['readme.rst','LICENSE'])]
     )
 elif sys.platform == 'win32':
     setuptools_setup(
-        name='shared_atomic',
-        version="2.0.0",
-        author="Xiquan Ren",
-        author_email="xiquanren@yandex.com",
-        description="Shared atomicity with multiprocessing/multithreads shared ctypes",
-        packages=['shared_atomic'],
+        name=__package_name__,
+        version=__version__,
+        author=__author__,
+        author_email=__author_email__,
+        description=__description__,
+        url=__url__,
+        long_description=readme,
+        packages=__packages__,
         python_requires=">=3.0",
         install_requires=[
             'cppyy ==2.3.1',
