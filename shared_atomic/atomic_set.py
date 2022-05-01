359b5d28518c808cefc060d2feb4ec90dcab16f5
Xiquan Ren <brightdayrxq@yahoo.com>
2022-05-01 23:19:11 +0800

    added set fixed bug in bytearray

---
+++ shared_atomic/atomic_set.py
@@ -0,0 +1,414 @@
+import ctypes
+from ctypes import byref
+from ctypes import c_uint as uint
+from ctypes import c_ulonglong as ulonglong
+
+from multiprocessing import Value
+from shared_atomic import loaddll
+from typing import Iterable
+import math
+import sys
+from bitarray import bitarray
+from bitarray.util import ba2hex, ba2int, int2ba
+from tensorboard.backend.application import DATA_PREFIX
+
+class atomic_set:
+    """
+    set provide atomic operations, the set should be no longer than 8 bytes
+    """
+
+    def __init__(self, initial: Iterable,
+                 encoding='utf-8',
+                 mode='singleprocessing'):
+        r"""
+        constructor to initialize the set,
+        the set should be no longer than 8 bytes
+
+        :param initial: initial value of the set, if the initial value is longer than 8 bytes, please specify the trimming target length, or else it would fail.
+        :param mode: the mode in which the set will be shared. 'singleprocessing' or 's' for single process, 'multiprocessing' or 'm' for multiprocessing, on windows platform, only singleprocessing is supported, setting it to 'm' or 'multiprocessing' will be ignored.
+        :param length: the expected length after padding/trimming for the input value, if not specified, no padding or trimming performed, use original value.
+        :param paddingdirection: right, or left side the padding bytes would be added if not specified, pad to the right side, use 'right' or 'r' to specify right side, use 'left' or 'l' to specify the left side.
+        :param paddingbytes: bytes to pad to the original bytes, by default b'\\0' can be multiple bytes like b'ab', will be padded to the original bytes in circulation until the expected length is reached.
+        :param trimming_direction: if initial bytes are longer, on which side the bytes will be trimmed. By default, on the right side, use 'right' or 'r' to specify right side, use 'left' or 'l' to specify the left side.
+        """
+        self.encoding = encoding
+        atomic = loaddll()
+        self.atomic = atomic
+        initial_set = set(initial)
+
+        full_data, self.initial_length = self.encode(initial_set)
+
+        if self.initial_length <= 4:
+            self.type = ctypes.c_uint
+            self.size = 4
+            self._array_store = atomic.uint_store
+            self._array_get_and_set = atomic.uint_get_and_set
+            self._array_shift = atomic.uint_shift
+            self._array_compare_and_set = atomic.uint_compare_and_set
+            self._array_add_and_fetch = atomic.uint_add_and_fetch
+            self._array_sub_and_fetch = atomic.uint_sub_and_fetch
+            self._array_and_and_fetch = atomic.uint_and_and_fetch
+            self._array_or_and_fetch = atomic.uint_or_and_fetch
+            self._array_xor_and_fetch = atomic.uint_xor_and_fetch
+            self._array_nand_and_fetch = atomic.uint_nand_and_fetch
+            self._array_fetch_and_add = atomic.uint_fetch_and_add
+            self._array_fetch_and_sub = atomic.uint_fetch_and_sub
+            self._array_fetch_and_and = atomic.uint_fetch_and_and
+            self._array_fetch_and_or = atomic.uint_fetch_and_or
+            self._array_fetch_and_xor = atomic.uint_fetch_and_xor
+            self._array_fetch_and_nand = atomic.uint_fetch_and_nand
+        elif self.initial_length <= 8:
+            self.type = ctypes.c_ulonglong
+            self.size = 8
+            self._array_store = atomic.ulonglong_store
+            self._array_get_and_set = atomic.ulonglong_get_and_set
+            self._array_shift = atomic.ulonglong_shift
+            self._array_compare_and_set = atomic.ulonglong_compare_and_set
+            self._array_add_and_fetch = atomic.ulonglong_add_and_fetch
+            self._array_sub_and_fetch = atomic.ulonglong_sub_and_fetch
+            self._array_and_and_fetch = atomic.ulonglong_and_and_fetch
+            self._array_or_and_fetch = atomic.ulonglong_or_and_fetch
+            self._array_xor_and_fetch = atomic.ulonglong_xor_and_fetch
+            self._array_nand_and_fetch = atomic.ulonglong_nand_and_fetch
+            self._array_fetch_and_add = atomic.ulonglong_fetch_and_add
+            self._array_fetch_and_sub = atomic.ulonglong_fetch_and_sub
+            self._array_fetch_and_and = atomic.ulonglong_fetch_and_and
+            self._array_fetch_and_or = atomic.ulonglong_fetch_and_or
+            self._array_fetch_and_xor = atomic.ulonglong_fetch_and_xor
+            self._array_fetch_and_nand = atomic.ulonglong_fetch_and_nand
+
+        else:
+            raise ValueError("Input set too long! It should be no more than 8 bytes!")
+
+        if sys.platform in ('darwin','linux'):
+            if mode in ('m', 'multiprocessing'):
+                self.mode = 'm'
+                self.array = Value(self.type, lock=False)
+            elif mode in ('s', 'singleprocessing'):
+                self.mode = 's'
+                self.array = self.type(0)
+            self.array_reference = byref(self.array)
+            #print(self.type(full_data))
+            #print(self.type(full_data).value)
+
+            self._array_get_and_set(self.array_reference, self.type(full_data))
+
+        elif sys.platform == 'win32':
+            self.mode = 's'
+            data = full_data.to_bytes(length=8, byteorder='big')
+            self.array = bytearray(data)
+            self.array_reference = memoryview(self.array)
+
+    def decode(self, bits_in_bytes: bytes)->set:
+        """Function to decode the bytes to set
+
+        :param bits_in_bytes:  bytes needs to be decoded
+        :param encoding: character encoding
+        :return: the decoded set
+        """
+        bits = bitarray()
+        bits.frombytes(bits_in_bytes)
+        data_continue_flag = True
+        i = uint(0)
+        meta_list = []
+        while not bits[0]:
+            bits <<= 1
+        bits <<= 1
+        while data_continue_flag:
+            kind = ba2int(bits[i.value:i.value+2])
+            if kind != 0:
+                length = ba2int(bits[i.value+2:i.value+5])
+                meta_list.append({'length': length, 'kind': kind})
+                if not bits[i.value+5]:
+                    data_continue_flag = False
+                self.atomic.uint_add_and_fetch(byref(i),uint(6))
+
+            else:
+                meta_list.append({'length': 1, 'kind': 0})
+                if not bits[i.value+2]:
+                    data_continue_flag = False
+                self.atomic.uint_add_and_fetch(byref(i),uint(3))
+
+        result = set()
+
+        for data_dict in meta_list:
+            start_point = i.value
+            kind = data_dict['kind']
+            length = data_dict['length']
+
+            if kind == 0:
+                result.add(True if bits[start_point] else False)
+                self.atomic.uint_add_and_fetch(byref(i),uint(1))
+            elif kind == 1:
+                result.add(ba2int(bits[start_point:start_point+length]))
+                self.atomic.uint_add_and_fetch(byref(i),uint(length))
+            elif kind == 2:
+                str_bytes = bytes.fromhex(ba2hex(bits[start_point:start_point + length*8]))
+                result.add(str_bytes.decode(self.encoding))
+                self.atomic.uint_add_and_fetch(byref(i),uint(length*8))
+            elif kind == 3:
+                result.add(bytes.fromhex(ba2hex(bits[start_point:start_point + length*8])))
+                self.atomic.uint_add_and_fetch(byref(i),uint(length*8))
+            else:
+                raise ('Type not supported!')
+        return result
+
+    def encode(self, input_set: set) -> (int, int):
+        """function to encode the input_set with specific character encoding
+
+        :param input_set: input set
+        :param encoding: character encoding
+        :return: (data in integer representation, total length in bits)
+        """
+        accumulated_length = uint(0)
+        item_num = uint(0)
+        data_bitarray = bitarray()
+        data_prefix = ulonglong(1)
+        for i in input_set:
+            hash(i)
+            if item_num.value != 0:
+                self.atomic.uint_add_and_fetch(byref(data_prefix), uint(1))
+            self.atomic.uint_add_and_fetch(byref(item_num), uint(1))
+
+            if isinstance(i, bool):
+                length = uint(1)
+                data_prefix, accumulated_length = self.shift_and_add(data_prefix,
+                                                                     accumulated_length,
+                                                                     length, 0)
+                data_bitarray += [i]
+            elif isinstance(i, int):
+                #length = uint(max(math.ceil(math.log2(i+1)),1))
+                b = int2ba(i)
+                length = uint(len(b))
+                data_prefix, accumulated_length = self.shift_and_add(data_prefix,
+                                                                     accumulated_length,
+                                                                     length, 1)
+                
+                #b.frombytes(i.to_bytes(length=max(math.ceil(length.value/8),1), byteorder='big'))
+                #c = b[-length.value:]
+                data_bitarray += b
+
+            elif isinstance(i, str):
+                input_bytes = i.encode(encoding=self.encoding)
+                length = uint(len(input_bytes))
+                data_prefix, accumulated_length = self.shift_and_add(data_prefix,
+                                                                     accumulated_length,
+                                                                     length, 2)
+                b = bitarray()
+                b.frombytes(input_bytes)
+                data_bitarray += b
+            elif isinstance(i, bytes):
+                length = uint(len(i))
+                data_prefix, accumulated_length = self.shift_and_add(data_prefix,
+                                                                     accumulated_length,
+                                                                     length, 3)
+                b = bitarray()
+                b.frombytes(i)
+                data_bitarray += b
+            else:
+                raise ('Type not supported!')
+
+        total_length = math.ceil((len(f'{data_prefix.value:b}') + accumulated_length.value) / 8)
+        if total_length > 8:
+            raise ValueError("Total Length exceeds 8 bytes!")
+        #data_int = int.from_bytes(data_bitarray.tobytes(), byteorder='big')
+        data_int = ba2int(data_bitarray)
+        full_data = (data_prefix.value << len(data_bitarray)) + data_int
+        return full_data, total_length
+
+    def shift_and_add(self, data_prefix: ulonglong, accumulate_length: uint, input_length:uint, kind:int) -> (uint, uint):
+        """Function to finish the shift and add of the data prefix
+
+        :param data_prefix: long long integer for the data_prefix
+        :param accumulate_length: total data length in bits accumulated so far
+        :param input_length: length of the data segment in bits
+        :param kind: sorts of data
+        :return: (data_prefix, accumulate_length)
+        """
+
+        if kind in (2,3):
+            self.atomic.uint_add_and_fetch(byref(accumulate_length), uint(input_length.value*8))
+        else:
+            self.atomic.uint_add_and_fetch(byref(accumulate_length), input_length)
+            
+        #binary_data_prefix = f'{data_prefix.value:b}'
+        #if binary_data_prefix[0] == '1' and len(binary_data_prefix) >= 64 :
+        #    raise ValueError("Numeric Overflow, cannot hold the highest bit!")
+
+        if kind != 0:
+            data_prefix.value <<= 6
+            self.atomic.uint_add_and_fetch(byref(data_prefix), (kind << 4) + (input_length.value << 1))
+        else:
+            data_prefix.value <<= 3
+            #self.atomic.uint_add_and_fetch(byref(data_prefix),0)
+
+        return data_prefix, accumulate_length
+
+    def get_int(self):
+        """
+        Get the whole integer representation from the set,
+        the whole array would be treated as a large integer
+
+        :return: the integer representation
+        """
+        result = self.type(0)
+        self._array_store(byref(result), self.array_reference)
+        return result.value
+
+    def set_int(self, integer: int):
+        """
+        Get the whole integer representation from the set,
+        the whole array would be treated as a large integer
+
+        :return: the integer representation
+        """
+        result = self.type(integer)
+        self._array_store(self.array_reference, byref(result))
+
+    def get_set(self):
+        r"""
+        Get all the bytes from the set atomically
+        :param trim: if True, the leading b'\\0' would be trimmed, by default: True
+
+        :return: all the bytes in the set
+        """
+        result = self.type(0)
+        self._array_store(byref(result), self.array_reference)
+        result = int.to_bytes(result.value, length=self.size, byteorder='big')
+        return self.decode(result)
+
+    def set_set(self, data: set):
+        """
+        Set the value in the set,
+        if the new data is longer than the original size of the array.
+        it will expand the array accordingly which would lose atomicy.
+        the size of the set can be check with self.size
+
+        :param data: input set
+        :return: None
+        """
+
+        integer, desiredlength = self.encode(data)
+
+        if 8 >= desiredlength > self.size:
+            self.__init__({b'\0\0\0\0\0\0'}, mode=self.mode, encoding=self.encoding)
+        #elif desiredlength > 8:
+        #    raise ValueError("Input data set too large cannot hold!")
+        ctype_integer = self.type(integer)
+        self._array_store(self.array_reference, byref(ctype_integer))
+
+
+    value = property(fget=get_set, fset=set_set, doc="same with get_set and set_set")
+    int_value = property(fget=get_int, doc="same with get_int")
+
+    def set_store(self, i):
+        """
+        Atomically store contents from another set to the this set,
+        if the other set is different with this one in size , the function will fail.
+
+        :param i: another set to store its value to self
+        :return: None
+        """
+        if self.size != i.size:
+            raise ValueError("Input set has different size!")
+        self._array_store(self.array_reference, i.array_reference)
+
+    def set_get_and_set(self, data: set):
+        r"""
+        Get and set atomically
+
+        :param data: new data
+        :param encoding: characterset
+        :return: the original bytes
+        """
+        integer, length = self.encode(data)
+        #if length > self.size:
+        #    raise ValueError("Input data too long cannot be set atomically!")
+        result = int.to_bytes(self._array_get_and_set(self.array_reference, self.type(integer)),
+                              length=self.size, byteorder='big')
+        return self.decode(result)
+
+    def set_shift(self, i, j):
+        """
+        Value exchange between 3 pointers in 2 groups atomically,
+        store i in itself after store itself in j
+
+        :param i: one atomic_set
+        :param j: another atomic_set
+        :return: None
+        """
+        if self.size != i.size:
+            raise ValueError("Comparing set i has different size!")
+        if self.size != j.size:
+            raise ValueError("Comparing set j has different size!")
+        self._array_shift(self.array_reference,
+                          i.array_reference, j.array_reference)
+
+    def set_compare_and_set(self, i, n: set) -> bool:
+        """
+        Compare and set atomically,This compares the contents of self
+        with the contents of i. If equal, the operation is a read-modify-write
+        operation that writes n into self. If they are not equal,
+        the operation is a read and the current contents of itself are written into
+        i.
+
+        :param i: the set to be compared with
+        :param n: another bytes to be ready to set to self if comparision return True
+        :param encoding: characterset
+        :return: if self is equal to i return True, else return False
+        """
+        if self.size != i.size:
+            raise ValueError("Comparing string has different size!")
+        integer, length = self.encode(n)
+        #if length > self.size:
+        #    raise ValueError("Input data too large, cannot set atomically!")
+        result = self._array_compare_and_set(self.array_reference,
+                                             i.array_reference, self.type(integer))
+        return result
+
+    def reencode(self, newencode: str):
+        """
+        Change the encoding of the string, if the original size is not enough,
+        it will elongate the string, if 7 bytes are not enough, it will fail.
+
+        :param newencode: new encoding, such as 'utf-8', 'utf-16-le'
+        :return: None
+        """
+        data = self.get_set()
+        prev_encoding = self.encoding
+        self.encoding = newencode
+        try:
+            self.set_set(data)
+        except ValueError:
+            self.encoding = prev_encoding
+            self.set_set(data)
+            raise ValueError("New encoding took larger space than 8 bytes, cannot elongate!")
+
+
+    if sys.platform in ('darwin','linux'):
+        def change_mode(self, newmode='m'):
+            """
+            Switch between singleprocessing mode and multiprocessing mode,
+            the function doesn't exists on windows, since only single processing mode
+            is supported on windows platform.
+            the contents will be copied , other threads/processes would not be aware of the change.
+
+            :param newmode: the mode to change to, 'm' or 'multiprocessing' for multiproessing, 's' or 'singleprocessing' for singleprocessing. default 'm'
+            :return: None
+            """
+            if newmode in ('m', 'multiprocessing'):
+                if self.mode == 's':
+                    data = self.get_int()
+                    self.array = Value(ctypes.c_ubyte, self.size, lock=False)
+                    self.array_reference = byref(self.array)
+                    self.set_int(data)
+
+            elif newmode not in ('s', 'singleprocessing'):
+                raise ValueError("newmode has the wrong value, should be 'm','s','multiprocessing' or 'singleprocessing'")
+
+            elif self.mode == 'm':
+                data = self.get_int()
+                self.array = self.type(0)
+                self.array_reference = byref(self.array)
+                self.set_int(data)
+            self.mode = newmode
---
+++ shared_atomic/test_atomic_set.py
@@ -0,0 +1,160 @@
+from shared_atomic import atomic_set
+from shared_atomic import loaddll
+import ctypes
+import sys
+from threading import Thread
+from multiprocessing import Process, Value
+
+
+inlist = (
+         'a',
+         'é',
+         '重1',
+         '重重',
+)
+
+
+exlist = ('b',
+         'è',
+         '启2',
+         '轻轻',
+          )
+
+atomic = loaddll()
+
+def signed2unsigned(input, i):
+    if input < 0:
+        return int.to_bytes(input + 2**((i+1)*8),length=i+1, byteorder='big').lstrip(b'\0')
+    return int.to_bytes(input,length=2**i, byteorder='big').lstrip(b'\0')
+
+
+def setup_function():
+    """
+    pre function for pytest
+    :return: None
+    """
+    global atomic
+    atomic = loaddll()
+    # if sys.platform in ('darwin','linux'):
+    #     dlltype = ctypes.CDLL
+    #     os.chdir('/Users/philren/.local/share/virtualenvs/spark-examples--HrH57AW/lib/python3.6/site-packages')
+    #     filename = 'shared_atomic.cpython-36m-darwin.so'
+    # elif sys.platform == "win32":
+    #     dlltype = ctypes.windll
+    # else:
+    #     return
+    #atomic = ctypes.LibraryLoader(dlltype).LoadLibrary(filename)
+
+def teardown_function():
+    pass
+
+def test_init():
+    a = atomic_set({1, 2, 3})
+    assert a.get_set() == {1,2,3}
+    a = atomic_set((True, False,))
+    assert a.get_set() == {True, False}
+    a = atomic_set({b'ab',b'cd'})
+    assert a.get_set() == {b'ab',b'cd'}
+    a = atomic_set({'ab','cd'})
+    assert a.get_set() == {'ab','cd'}
+    a = atomic_set({'中','国'},encoding='utf-16-le')
+    assert a.get_set(encoding='utf-16-le') == {'中','国'}
+
+    a = atomic_set({1,'中',True,b'x12'}, encoding='utf-16-le')
+    assert a.get_set(encoding='utf-16-le') == {1,'中',True,b'x12'}
+
+def test_set():
+    a = atomic_set({1,'中',True,b'x12'}, encoding='utf-16-le')
+    assert a.get_set() == {1,'中',True,b'x12'}
+    a = atomic_set({1}, encoding='utf-16-le')
+    a.set_set({0,'国',True,b'x21'})
+    assert a.get_set() == {0,'国',True,b'x21'}
+    b = atomic_set({0,'国',True,b'x12'}, encoding='utf-16-le')
+
+    result = a.set_compare_and_set(b, {1,2,3})
+
+    assert result == False
+    assert b.get_set() == a.get_set()
+
+    result = a.set_compare_and_set(b, {0,1,2,3,4,5,6})
+
+    assert result == True
+    assert a.get_set() == {0,1,2,3,4,5,6}
+
+    a.set_set({0,'国',True,b'x2'})
+
+    a.reencode('utf-8')
+
+    a.value = {0,'国',True,b'x2'}
+
+    b = atomic_set({12,'s',True,b'x12'})
+    a.set_store(b)
+
+    assert a.value == b.value
+
+    result = a.set_get_and_set({0,1,2,3})
+    assert result == {12,'s',True,b'x12'}
+    assert a.value == {0,1,2,3}
+
+    c = atomic_set({12,'s',3,4,5})
+    b = atomic_set({0,1,4,3,2,5,6})
+    a.set_shift(b, c)
+    assert a.value == {0,1,4,3,2,5,6}
+    assert c.value == {0,1,2,3}
+
+def thread_run(a,i):
+
+    b = atomic_set({'ab'})
+    if a.set_compare_and_set(b, {'cd'}):
+        atomic.long_add_and_fetch(ctypes.byref(i), ctypes.c_long(1))
+
+def test_thread_atomic():
+    """
+    test single process multiple threads
+    :return: None
+    """
+    a = atomic_set({'ab'})
+    b = ctypes.c_long(0)
+
+    threadlist = []
+
+    for i in range(10000):
+        threadlist.append(Thread(target=thread_run, args=(a, b)))
+
+    for i in range(10000):
+        threadlist[i].start()
+
+    for i in range(10000):
+        threadlist[i].join()
+
+    assert a.value == {'cd'}
+    assert b.value == 1
+
+
+if sys.platform != "win32":
+    def process_run(a,c):
+        b = atomic_set({'ab'})
+        if a.set_compare_and_set(b, {'cd'}):
+            atomic.long_add_and_fetch(ctypes.byref(c), ctypes.c_long(1))
+
+    def test_process_atomic():
+        """
+        test multiple processes
+        :return: None
+        """
+        a = atomic_set({'ab'}, mode='m')
+        c = Value(ctypes.c_long, lock=False)
+        c.value = 0
+        processlist = []
+        for i in range(10000):
+            processlist.append(Process(target=process_run, args=(a,c)))
+
+        for i in range(10000):
+            processlist[i].start()
+
+        for i in range(10000):
+            processlist[i].join()
+
+        assert a.value == {'cd'}
+        assert c.value == 1
+
