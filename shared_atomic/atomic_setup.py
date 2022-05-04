9b5b60da2363f62040075c164621830232fd7ec2
Xiquan Ren <brightdayrxq@yahoo.com>
2022-05-04 20:05:26 +0800

    fixed issue on ubuntu

--- shared_atomic/atomic_setup.py
+++ shared_atomic/atomic_setup.py
@@ -19,21 +19,22 @@ cfilepath = Path.joinpath(Path(__file__).parent, 'atomic_csource.c')
 
 if sys.platform == 'linux':
     header = \
-'typedef long int off_t;\
-#define PROT_READ       0x1             /* page can be read */\
-#define PROT_WRITE      0x2             /* page can be written */\
-#define PROT_EXEC       0x4             /* page can be executed */\
-#define PROT_SEM        0x8             /* page may be used for atomic ops */\
-#define PROT_NONE       0x0             /* page can not be accessed */\
-#define PROT_GROWSDOWN  0x01000000      /* mprotect flag: extend change to start of growsdown vma */\
-#define PROT_GROWSUP    0x02000000      /* mprotect flag: extend change to end of growsup vma */\
-\
-#define MAP_SHARED      0x01            /* Share changes */\
-#define MAP_PRIVATE     0x02            /* Changes are private */\
-#define MAP_SHARED_VALIDATE 0x03        /* share + validate extension flags */\
-#define MAP_TYPE        0x0f            /* Mask for type of mapping */\
-#define MAP_FIXED       0x10            /* Interpret addr exactly */\
-#define MAP_ANONYMOUS   0x20            /* don\'t use a file */'
+'''typedef long int off_t;
+#define PROT_READ       0x1             /* page can be read */
+#define PROT_WRITE      0x2             /* page can be written */
+#define PROT_EXEC       0x4             /* page can be executed */
+#define PROT_SEM        0x8             /* page may be used for atomic ops */
+#define PROT_NONE       0x0             /* page can not be accessed */
+#define PROT_GROWSDOWN  0x01000000      /* mprotect flag: extend change to start of growsdown vma */
+#define PROT_GROWSUP    0x02000000      /* mprotect flag: extend change to end of growsup vma */
+
+#define MAP_SHARED      0x01            /* Share changes */
+#define MAP_PRIVATE     0x02            /* Changes are private */
+#define MAP_SHARED_VALIDATE 0x03        /* share + validate extension flags */
+#define MAP_TYPE        0x0f            /* Mask for type of mapping */
+#define MAP_FIXED       0x10            /* Interpret addr exactly */
+#define MAP_ANONYMOUS   0x20            /* don\'t use a file */
+'''
 
 elif sys.platform == 'darwin':
     header = 'typedef long long off_t;\n'
