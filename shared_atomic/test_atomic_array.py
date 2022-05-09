d624cbf62c1c56eb8dc1205e7adf4dd2cb23fa80
Xiquan Ren <brightdayrxq@yahoo.com>
2022-05-09 18:16:43 +0800

    updated the threading processes model

--- shared_atomic/test_atomic_array.py
+++ shared_atomic/test_atomic_array.py
@@ -244,7 +244,19 @@ def test_thread_atomic():
 
 if sys.platform != "win32":
     def process_run(a):
-        a.array_sub_and_fetch(b'\x0F')
+
+        def subthread_run(a):
+            a.array_sub_and_fetch(b'\x0F')
+
+        threadlist = []
+        for t in range(5000):
+            threadlist.append(Thread(target=subthread_run, args=(a,)))
+
+        for t in range(5000):
+            threadlist[t].start()
+
+        for t in range(5000):
+            threadlist[t].join()
 
     def test_process_atomic():
         """
@@ -254,14 +266,14 @@ if sys.platform != "win32":
         a = atomic_bytearray(b'ab', length=7, paddingdirection='r', paddingbytes=b'012', mode='m')
 
         processlist = []
-        for i in range(10000):
+        for p in range(2):
             processlist.append(Process(target=process_run, args=(a,)))
 
-        for i in range(10000):
-            processlist[i].start()
+        for p in range(2):
+            processlist[p].start()
 
-        for i in range(10000):
-            processlist[i].join()
+        for p in range(2):
+            processlist[p].join()
 
         assert a.value == int.to_bytes(27411031864108609,length=8,byteorder='big')
 
